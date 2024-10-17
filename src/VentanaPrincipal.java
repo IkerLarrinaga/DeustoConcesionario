c666aa7e62da9fbf4c155293e3ec1f9e59fa16ca
iñigo calderon <i.calderon@opendeusto.es>
2024-10-17 15:31:10 +0200

    Creada la Ventana de la clase VentanaPrincipañ

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -1,9 +1,15 @@
+import javax.swing.JFrame;
 
 public class VentanaPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Ventana Principal");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        
+        
+        frame.setVisible(true);
 	}
 
 }
