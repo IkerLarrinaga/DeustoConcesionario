0016e8d8c2f2aa1c6fcd34483965cbe7bd379af8
HugoReyIn <hugo.rey@opendeusto.es>
2024-10-17 15:43:52 +0200

    Commit de Prueba

--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -7,6 +7,7 @@ public class VentanaPrincipal {
 		JFrame frame = new JFrame("Ventana Principal");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 200);
+        //MENSAJE DE PRUEBA
         
         
         frame.setVisible(true);
