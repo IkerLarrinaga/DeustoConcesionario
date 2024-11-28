3f42bec9c943769eca91a94706a39569ade59515
HugoReyIn <hugo.rey@opendeusto.es>
2024-11-28 15:13:51 +0100

    Eliminados archivos innecesarios

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,11 @@
 package main;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 
 import gui.VentanaIncio;
 
-
 public class Main extends JFrame {
 	private static final long serialVersionUID = 1L;
-	
-	static JFrame frame = new JFrame("Cargando...");
-	static JPanel panel = new JPanel();
-	static JProgressBar progressBar = new JProgressBar();
 	
     public static void main(String[] args) {    	
     	new VentanaIncio();
