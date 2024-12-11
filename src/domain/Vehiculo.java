035a289b3a061587d51c5d28dd5a275a95600663
HugoReyIn <hugo.rey@opendeusto.es>
2024-12-11 15:10:12 +0100

    Quitados warnings

--- src/domain/Vehiculo.java
+++ src/domain/Vehiculo.java
@@ -3,15 +3,8 @@ package domain;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import db.DataBaseManager;
 
 
 public abstract class Vehiculo {
